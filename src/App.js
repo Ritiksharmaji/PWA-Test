5fc3ec18f2dd170904aaa064fca81314b2d7984d
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-06-16 17:27:06 +0530

    adding notification

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchWeather } from './api/fetchWeather';
 import { APP_VERSION } from './version';
 import './App.css';
@@ -7,6 +7,26 @@ export default function App() {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
 
+
+    useEffect(() => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistration().then((registration) => {
+            if (registration && registration.waiting) {
+                showUpdateNotification(); // Already waiting
+            }
+
+            registration && registration.addEventListener('updatefound', () => {
+                const newWorker = registration.installing;
+                newWorker && newWorker.addEventListener('statechange', () => {
+                    if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                        showUpdateNotification(); // New update ready
+                    }
+                });
+            });
+        });
+    }
+}, []);
+
     const search = async (e) => {
         if (e.key === 'Enter') {
             const data = await fetchWeather(query);
@@ -14,6 +34,14 @@ export default function App() {
             setQuery('');
         }
     };
+
+    
+const showUpdateNotification = () => {
+    const confirmUpdate = window.confirm("🚀 A new version is available. Do you want to update?");
+    if (confirmUpdate) {
+        window.location.reload(true); // Reload and activate new service worker
+    }
+};
 
     return (
         <div className="main-container">
@@ -43,9 +71,12 @@ export default function App() {
                             alt={weather.weather[0].description}
                         />
                         <p>{weather.weather[0].description}</p>
+                        
                     </div>
+                    
                 </div>
             )}
+            <p>this is for version check with notification</p>
 
             <footer className="app-version">Version: {APP_VERSION}</footer>
         </div>
